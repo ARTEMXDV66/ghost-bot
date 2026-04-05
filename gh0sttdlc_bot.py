beabc304575203eafb210dafc5b6fa2960178d50
ARTEMXDV66 <artemtchzhin@mail.ru>
2026-04-05 14:59:08 +0300

    Update gh0sttdlc_bot.py

--- gh0sttdlc_bot.py
+++ gh0sttdlc_bot.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
-BOT_TOKEN = "8747534538:AAHtj6NUcH2DNHPCeDcyQh2apDqSI5u9fuU"
+BOT_TOKEN = "8747534538:AAGGC77iG60mICeiOt2-JzJnzcDbwos0frA"
 WALLET_NUMBER = "4100118548432704"
 SECRET_KEY = "TZVRhM5F+MCqSMWeLUtPrEyL"
 APK_LINK = "https://t.me/zjTfte-9i282MmQy"
